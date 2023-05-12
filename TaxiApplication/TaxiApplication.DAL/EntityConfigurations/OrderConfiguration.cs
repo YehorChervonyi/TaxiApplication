92f9e3cdb46e7d0cf0bc19e0fa61918ec25fe369
Yehor Chervony <chervony.egor@gmail.com>
2023-05-13 02:21:17 +0300

    Added entities configurations for (base, car, driver, Order, tariff, user) entities

---
+++ TaxiApplication/TaxiApplication.DAL/EntityConfigurations/OrderConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Data.Entities;
+
+namespace Data.EntityConfigurations;
+
+public class OrderConfiguration : IEntityTypeConfiguration<Order>
+{
+    public void Configure(EntityTypeBuilder<Order> builder)
+    {
+        builder.HasIndex(x => x.id);
+        builder.Property(x => x.id).IsRequired();
+
+        builder.Property(x => x.user).IsRequired();
+        
+        builder.Property(x => x.start).IsRequired();
+        builder.Property(x => x.start).HasMaxLength(500);
+        
+        builder.Property(x => x.finish).IsRequired();
+        builder.Property(x => x.finish).HasMaxLength(500);
+        
+        builder.Property(x => x.status).IsRequired();
+
+        builder
+            .HasOne(x => x.user)
+            .WithMany(x => x.orders)
+            .HasForeignKey(x=>x.userId)
+            .HasPrincipalKey(x=>x.id);
+
+        builder
+            .HasOne(x => x.driver)
+            .WithMany(x => x.orders)
+            .HasForeignKey(x=>x.driverId)
+            .HasPrincipalKey(x=>x.id);
+    }
+}
