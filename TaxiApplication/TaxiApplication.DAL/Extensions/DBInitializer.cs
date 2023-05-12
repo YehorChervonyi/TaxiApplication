be11ecc414fb6fe70e9c24eb15f31db130d69d02
Yehor Chervony <chervony.egor@gmail.com>
2023-05-13 02:23:49 +0300

    Added extensions (DBInitializer, Injecting)

---
+++ TaxiApplication/TaxiApplication.DAL/Extensions/DBInitializer.cs
@@ -0,0 +1,11 @@
+﻿using Data.Context;
+
+namespace Data.Entities;
+
+public class DBInitializer
+{
+    public static void Initialize(DBContext context)
+    {
+        context.Database.EnsureCreated();
+    }
+}
---
+++ TaxiApplication/TaxiApplication.DAL/Extensions/Injecting.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Data.Entities;
+
+public static class Injecting
+{
+    public static void Inject(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContext<Data.Context.DBContext>(options =>
+        {
+            options.UseSqlServer(
+                configuration["ConnectionString"]);
+        });
+    }
+}
