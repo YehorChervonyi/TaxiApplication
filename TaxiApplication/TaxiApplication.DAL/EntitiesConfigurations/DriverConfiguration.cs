e5af26e4abf8187d37e3dc98bb96cc9891ece652
Yehor Chervony <chervony.egor@gmail.com>
2023-05-14 01:38:10 +0300

    Created entities configurations for car, driver, order, tariff, user entities

---
+++ TaxiApplication/TaxiApplication.DAL/EntitiesConfigurations/DriverConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TaxiApplication.DAL.Entities;
+
+namespace TaxiApplication.DAL.EntitiesConfigurations;
+
+public class DriverConfiguration: IEntityTypeConfiguration<Driver>
+{
+    public void Configure(EntityTypeBuilder<Driver> builder)
+    {
+        builder.HasIndex(x => x.id);
+        builder.Property(x => x.id).IsRequired();
+        
+        builder.Property(x => x.name).IsRequired();
+        builder.Property(x => x.name).HasMaxLength(100);
+        
+        builder.Property(x => x.surname).IsRequired();
+        builder.Property(x => x.surname).HasMaxLength(100);
+    }
+}
---
+++ TaxiApplication/TaxiApplication.DAL/EntitiesConfigurations/TariffConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TaxiApplication.DAL.Entities;
+
+namespace TaxiApplication.DAL.EntitiesConfigurations;
+
+public class TariffConfiguration : IEntityTypeConfiguration<Tariff>
+{
+    public void Configure(EntityTypeBuilder<Tariff> builder)
+    {
+        builder.HasIndex(x => x.id);
+        builder.Property(x => x.id).IsRequired();
+
+        builder.Property(x => x.name).IsRequired();
+        builder.Property(x => x.name).HasMaxLength(100);
+        
+        builder.Property(x => x.price).IsRequired();
+        
+        builder.Property(x => x.fee).IsRequired();
+    }
+}
