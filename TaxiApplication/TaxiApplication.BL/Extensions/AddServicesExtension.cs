249a711edcbe3b56d7697d494cfa0b03f8c58d14
Yehor Chervony <chervony.egor@gmail.com>
2023-05-14 01:42:29 +0300

    Created extension for adding services

---
+++ TaxiApplication/TaxiApplication.BL/Extensions/AddServicesExtension.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TaxiApplication.BL.Mapping;
+using TaxiApplication.BL.Repository;
+using TaxiApplication.DAL.Extensions;
+
+namespace TaxiApplication.BL.Extensions;
+
+public static class AddServicesExtension
+{
+    public static void AddServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddScoped(typeof(IGenericRepository<>));
+
+        // services.AddScoped<IGenericRepository>();
+        services.AddScoped(typeof(Injecting));
+        services.AddAutoMapper(
+            config =>
+            {
+                config.AddProfile(typeof(AutoMapperProfile));
+            });
+    }
+}
