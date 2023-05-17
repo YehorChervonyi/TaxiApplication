cb072da04899e2cce24a103fc251afe98bca27e0
Yehor Chervony <chervony.egor@gmail.com>
2023-05-18 02:00:00 +0300

    Removed carId field

--- TaxiApplication/TaxiApplication.DAL/Entities/Driver.cs
+++ TaxiApplication/TaxiApplication.DAL/Entities/Driver.cs
@@ -5,8 +5,6 @@ public class Driver : BaseEntity
     public string name { get; set; }
     public string surname { get; set; }
     public int? experience { get; set; }
-    
-    public int? carId { get; set; }
 
     public virtual ICollection<Order>? orders { get; set; } = new HashSet<Order>();
     public virtual ICollection<Car>? cars { get; set; } = new HashSet<Car>();
