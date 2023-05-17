98c05be3ab1f2e0eecd37793c91a5c3e846676c6
Yehor Chervony <chervony.egor@gmail.com>
2023-05-18 02:24:25 +0300

    Created DTOs

---
+++ TaxiApplication/TaxiApplication/DTOs/DriverDto.cs
@@ -0,0 +1,8 @@
+﻿namespace TaxiApplication.DTOs;
+
+public class DriverDto
+{
+    public string name { get; set; }
+    public string surname { get; set; }
+    public int? experience { get; set; }
+}
---
+++ TaxiApplication/TaxiApplication/DTOs/TariffDto.cs
@@ -0,0 +1,8 @@
+﻿namespace TaxiApplication.DTOs;
+
+public class TariffDto
+{
+    public string name { get; set; }
+    public float  price { get; set; }
+    public float fee { get; set; }
+}
