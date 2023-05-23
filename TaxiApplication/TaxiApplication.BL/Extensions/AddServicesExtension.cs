774804e7677b7421dab8bb1f879f7f4f6c6a8b48
Yehor Chervony <chervony.egor@gmail.com>
2023-05-23 23:21:41 +0300

    Added Auth, jwt services, jwt token validation parameters

--- TaxiApplication/TaxiApplication.BL/Extensions/AddServicesExtension.cs
+++ TaxiApplication/TaxiApplication.BL/Extensions/AddServicesExtension.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using TaxiApplication.BL.Mapping;
 using TaxiApplication.BL.Services.Classes;
 using TaxiApplication.BL.Services.Interfaces;
@@ -7,7 +10,7 @@ namespace TaxiApplication.BL.Extensions;
 
 public static class AddServicesExtension
 {
-    public static void AddServices(this IServiceCollection services)
+    public static void AddServices(this IServiceCollection services, IConfiguration configuration)
     {
 
         services.AddScoped<ICarService, CarService>();
@@ -15,8 +18,27 @@ public static class AddServicesExtension
         services.AddScoped<IOrderService, OrderService>();
         services.AddScoped<ITariffService, TariffService>();
         services.AddScoped<IUserService, UserService>();
+
+        services.AddScoped<IAuthService, AuthService>();
+        
         
         
         services.AddAutoMapper(typeof(AutoMapperProfile));
+
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidIssuer = "Auth",
+                ValidateAudience = true,
+                ValidAudience = "Auth",
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(configuration["SecretKey"]))
+            };
+        });
+        services.AddAuthorization();
+        services.AddScoped<IJwtService, JwtService>();
     }
 }
