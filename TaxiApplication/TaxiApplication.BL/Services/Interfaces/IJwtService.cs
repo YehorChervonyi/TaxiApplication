3c195ca8a9f35a852b683ffb1249572b19f7c40d
Yehor Chervony <chervony.egor@gmail.com>
2023-05-23 23:19:29 +0300

    Added JWT, Auth services (Interfaces with implementation)

---
+++ TaxiApplication/TaxiApplication.BL/Services/Interfaces/IJwtService.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Configuration;
+using TaxiApplication.BL.Models;
+
+namespace TaxiApplication.BL.Services.Interfaces;
+
+public interface IJwtService
+{
+     string CreateJWT(UserModel user, IConfiguration configuration);
+     int GetId(string token);
+}
